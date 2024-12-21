b131a8f5b95d34b300efd096e343de1fb7446824
Ivan <imikon@proton.me>
2024-12-21 17:00:36 +0100

    finished with all processes of authorization, registration and logging out

--- src/lib/actions/user.actions.ts
+++ src/lib/actions/user.actions.ts
@@ -6,6 +6,7 @@ import { ID, Query } from "node-appwrite";
 import { parseStringify } from "@/lib/utils";
 import { cookies } from "next/headers";
 import { avatarPlaceholderUrl } from "../../../constants";
+import { redirect } from "next/navigation";
 
 //функция, которая делает запрос на получение пользователя по имэйлу
 const getUserByEmail = async (email: string) => {
@@ -36,8 +37,6 @@ export const sendEmailOTP = async ({ email }: { email: string }) => {
     handleError(error, "Failed to send email OTP");
   }
 };
-//функция для создания аккаунта. по сути мы "создаём" аккаунт каждый раз когда коннектимся к сервису
-//если пользователь по имэйлу существует, оно шлёт ему OTP и он заходит, иначе оно его регистрирует
 export const createAccount = async ({
   fullName,
   email,
@@ -110,4 +109,32 @@ export const getCurrentUser = async () => {
   if (user.total <= 0) return null;
 
   return parseStringify(user.documents[0]);
+};
+
+export const signOutUser = async () => {
+  const { account } = await createSessionClient();
+
+  try {
+    await account.deleteSession("current");
+    (await cookies()).delete("appwrite-session");
+  } catch (error) {
+    handleError(error, "Failed to log out user");
+  } finally {
+    redirect("/sign-in");
+  }
+};
+
+export const signInUser = async ({ email }: { email: string }) => {
+  try {
+    const existingUser = await getUserByEmail(email);
+
+    if (existingUser) {
+      await sendEmailOTP({ email });
+      return parseStringify({ accountId: existingUser.accountId });
+    }
+
+    return parseStringify({ accountId: null, error: "User not found" });
+  } catch (error) {
+    handleError(error, "Failed to log in");
+  }
 };
