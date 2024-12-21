aff3f8af213d6cb2060f69619125c22b8331ba74
Ivan <imikon@proton.me>
2024-12-21 19:05:50 +0100

    installed react-dropzone lib and started to implement uploading functionality. updated utils with a lot if useful functions and collections

--- src/components/FileUploader.tsx
+++ src/components/FileUploader.tsx
@@ -1,7 +1,64 @@
-import React from "react";
+"use client";
 
-const FileUploader = () => {
-  return <div>FileUploader</div>;
+import React, { useCallback, useState } from "react";
+import { useDropzone } from "react-dropzone";
+import { Button } from "@/components/ui/button";
+import { cn, getFileType } from "@/lib/utils";
+import Image from "next/image";
+
+interface Props {
+  ownerId: string;
+  accountId: string;
+  className?: string;
+}
+
+const FileUploader = ({ ownerId, accountId, className }: Props) => {
+  const [files, setFiles] = useState<File[]>([]);
+
+  const onDrop = useCallback(async (acceptedFiles: File[]) => {
+    setFiles(acceptedFiles);
+  }, []);
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+
+  return (
+    <div {...getRootProps()} className={"cursor-pointer"}>
+      <input {...getInputProps()} />
+      <Button type={"button"} className={cn("uploader-button", className)}>
+        <Image
+          src={"/assets/icons/upload.svg"}
+          alt={"upload"}
+          width={24}
+          height={24}
+        />
+        <p>Upload</p>
+      </Button>
+
+      {files.length > 0 && (
+        <ul className={"uploader-preview-list"}>
+          <h4 className={"h4 text-light-100"}>Uploading</h4>
+
+          {files.map((file, index) => {
+            const { type, extension } = getFileType(file.name);
+
+            return (
+              <li
+                key={`${file.name}-${index}`}
+                className={"uploader-preview-item"}
+              >
+                test
+              </li>
+            );
+          })}
+        </ul>
+      )}
+
+      {isDragActive ? (
+        <p>Drop the files here ...</p>
+      ) : (
+        <p>Drag &#39;n&#39; drop some files here, or click to select files</p>
+      )}
+    </div>
+  );
 };
 
 export default FileUploader;
