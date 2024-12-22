8a71213daa5571cd2876d08bd85001c5506d87d1
Ivan <imikon@proton.me>
2024-12-22 18:17:26 +0100

    created new server action for uploading files, also made some errors representation for user and files thumbnails

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -2,8 +2,9 @@ import React from "react";
 import Sidebar from "@/components/Sidebar";
 import MobileNavigation from "@/components/MobileNavigation";
 import Header from "@/components/Header";
-import { getCurrentUser } from "../../lib/actions/user.actions";
+import { getCurrentUser } from "@/lib/actions/user.actions";
 import { redirect } from "next/navigation";
+import { Toaster } from "@/components/ui/toaster";
 
 const Layout = async ({ children }: { children: React.ReactNode }) => {
   const currentUser = await getCurrentUser();
@@ -14,9 +15,12 @@ const Layout = async ({ children }: { children: React.ReactNode }) => {
     <main className={"flex h-screen"}>
       <Sidebar {...currentUser} />
       <section className={"flex h-full flex-1 flex-col"}>
-        <MobileNavigation {...currentUser} /> <Header />
+        <MobileNavigation {...currentUser} />
+        <Header userId={currentUser.$id} accountId={currentUser.accountId} />
         <div className={"main-content"}>{children}</div>
       </section>
+
+      <Toaster />
     </main>
   );
 };
--- src/components/FileUploader.tsx
+++ src/components/FileUploader.tsx
@@ -3,8 +3,13 @@
 import React, { useCallback, useState } from "react";
 import { useDropzone } from "react-dropzone";
 import { Button } from "@/components/ui/button";
-import { cn, getFileType } from "@/lib/utils";
+import { cn, convertFileToUrl, getFileType } from "@/lib/utils";
 import Image from "next/image";
+import Thumbnail from "@/components/Thumbnail";
+import { uploadFile } from "@/lib/actions/file.actions";
+import { MAX_FILE_SIZE } from "../../constants";
+import { useToast } from "@/hooks/use-toast";
+import { usePathname } from "next/navigation";
 
 interface Props {
   ownerId: string;
@@ -13,12 +18,51 @@ interface Props {
 }
 
 const FileUploader = ({ ownerId, accountId, className }: Props) => {
+  const path = usePathname();
+  const { toast } = useToast();
   const [files, setFiles] = useState<File[]>([]);
 
-  const onDrop = useCallback(async (acceptedFiles: File[]) => {
-    setFiles(acceptedFiles);
-  }, []);
-  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+  const onDrop = useCallback(
+    async (acceptedFiles: File[]) => {
+      setFiles(acceptedFiles);
+
+      const uploadPromises = acceptedFiles.map(async (file) => {
+        if (file.size > MAX_FILE_SIZE) {
+          setFiles((prev) => prev.filter((f) => f.name !== file.name));
+
+          return toast({
+            description: (
+              <p className={"body-2 text-white"}>
+                <span className={"font-semibold"}>{file.name}</span> is too
+                large. Max file size is 50MB
+              </p>
+            ),
+            className: "error-toast",
+          });
+        }
+
+        return uploadFile({ file, ownerId, accountId, path }).then(
+          (uploadedFile) => {
+            if (uploadedFile) {
+              setFiles((prev) => prev.filter((f) => f.name !== file.name));
+            }
+          },
+        );
+      });
+
+      await Promise.all(uploadPromises);
+    },
+    [ownerId, accountId, path],
+  );
+  const { getRootProps, getInputProps } = useDropzone({ onDrop });
+
+  const handleRemove = (
+    e: React.MouseEvent<HTMLImageElement, MouseEvent>,
+    fileName: string,
+  ) => {
+    e.stopPropagation();
+    setFiles((prev) => prev.filter((file) => file.name !== fileName));
+  };
 
   return (
     <div {...getRootProps()} className={"cursor-pointer"}>
@@ -45,17 +89,35 @@ const FileUploader = ({ ownerId, accountId, className }: Props) => {
                 key={`${file.name}-${index}`}
                 className={"uploader-preview-item"}
               >
-                test
+                <div className={"flex items-center gap-3"}>
+                  <Thumbnail
+                    type={type}
+                    extension={extension}
+                    url={convertFileToUrl(file)}
+                  />
+
+                  <div className={"preview-item-name"}>
+                    {file.name}
+                    <Image
+                      src={"/assets/icons/file-loader.gif"}
+                      alt={"loader"}
+                      width={80}
+                      height={26}
+                    />
+                  </div>
+                </div>
+
+                <Image
+                  src={"/assets/icons/remove.svg"}
+                  width={24}
+                  height={24}
+                  alt={"Remove"}
+                  onClick={(e) => handleRemove(e, file.name)}
+                />
               </li>
             );
           })}
         </ul>
-      )}
-
-      {isDragActive ? (
-        <p>Drop the files here ...</p>
-      ) : (
-        <p>Drag &#39;n&#39; drop some files here, or click to select files</p>
       )}
     </div>
   );
